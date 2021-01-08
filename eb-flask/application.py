5cc54a84cb926c3e8a23537fd58a93cf2bada13e
Loïc Diridollou <loic.diridollou@gmail.com>
2021-01-08 11:24:14 -0800

    Updating the CLI command for initializtion of db

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,16 +1,49 @@
 from flask import Flask, request, jsonify, abort, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
-from models import VehicleModel, setup_db
-from flask_mysqldb import MySQL
+from models import Vehicle, Utilization, User, setup_db
+import datetime as dt
+#from flask_mysqldb import MySQL
 
 
 application = Flask(__name__)
 db = setup_db(application)
 
-
-
 migrate = Migrate(application, db)
+
+@application.cli.command("initdb")
+def reset_db():
+    db.drop_all()
+    db.create_all()
+    
+    db.session.add(Vehicle(brand="Ferrari", model="F458", doors=3, vehicle_type="car"))
+    db.session.add(Vehicle(brand="Porsche", model="918 Spyder", doors=3, vehicle_type="car"))
+    db.session.add(Vehicle(brand="Citroën", model="C4 Picasso", doors=5, vehicle_type="car"))
+    db.session.add(Vehicle(brand="Volvo", model="XNR Electric", doors=2, vehicle_type="truck"))
+    db.session.add(Vehicle(brand="Freightliner", model="eCascadia", doors=2, vehicle_type="truck"))
+
+    u1 = User(username="jean_dupont", name="Jean Dupont", enrollment_time=dt.datetime(2020, 1, 5, 12, 30), level="manager")
+    u2 = User(username="marc_lhermitte", name="Marc L'Hermitte", enrollment_time=dt.datetime(2020, 1, 5, 12, 30), level="employee")
+    u3 = User(username="jeanmichel_serre", name="Jean-Michel Serre", enrollment_time=dt.datetime(2020, 1, 5, 12, 30), level="manager")
+    u4 = User(username="kevin_chan", name="Kevin Chan", enrollment_time=dt.datetime(2020, 1, 5, 12, 30), level="employee")
+    db.session.add(u1)
+    db.session.add(u2)
+    db.session.add(u3)
+    db.session.add(u4)
+    
+    util1 = Utilization(ref_vehicle=1, start_time=dt.datetime(2020, 6, 5, 12, 30), end_time=dt.datetime(2020, 6, 6, 12, 30))
+    db.session.add(util1)
+    util1.user_id.append(u2)
+    
+    
+    # db.session.add(Utilization(ref_vehicle=2, user_id=3, start_time=dt.datetime(2020, 6, 6, 12, 30), end_time=dt.datetime(2020, 6, 8, 12, 30)))
+    # db.session.add(Utilization(ref_vehicle=4, user_id=1, start_time=dt.datetime(2020, 6, 8, 12, 30), end_time=dt.datetime(2020, 6, 9, 12, 30)))
+
+    
+
+    db.session.commit()
+
+    
 
 @application.route('/')
 def hello():
@@ -18,8 +51,27 @@ def hello():
 
 
 @application.route('/vehicles')
-def cars():
-    cars = VehicleModel.query.all()
+def list_cars():
+    cars = Vehicle.query.all()
+    dic = {}
+    for car in cars:
+        dic[car.id] = {'brand': car.brand, 'model': car.model, 'num_doors': car.doors}
+    return jsonify(dic)
+
+
+@application.route('/utilizations')
+def list_utilizations():
+    utilizations = Utilization.query.all()
+    dic = {}
+    for use in utilizations:
+        dic[car.id] = {'vehicle': use.ref_vehicle, 'user': use.user_id, 'start_time': use.start_time, 'end_time': use.end_time}
+    return jsonify(dic)
+
+
+
+@application.route('/users')
+def list_users():
+    cars = Vehicle.query.all()
     dic = {}
     for car in cars:
         dic[car.id] = {'brand': car.brand, 'model': car.model, 'num_doors': car.doors}
@@ -28,7 +80,7 @@ def cars():
 
 @application.route('/vehicles/<string:vehicle_type>')
 def vehicles_by_type(vehicle_type):
-    vehicles = VehicleModel.query.filter(VehicleModel.vehicle_type==vehicle_type).all()
+    vehicles = Vehicle.query.filter(Vehicle.vehicle_type==vehicle_type).all()
     dic = {}
     for vehicle in vehicles:
         dic[vehicle.id] = {'brand': vehicle.brand, 'model': vehicle.model, 'num_doors': vehicle.doors, 'vehicle_type': vehicle.vehicle_type}
@@ -45,7 +97,7 @@ def add_cars():
     new_type = body.get('type', None)
 
     try:
-        vehicle = VehicleModel(brand=new_brand, model=new_model, doors=new_doors, vehicle_type=new_type)
+        vehicle = Vehicle(brand=new_brand, model=new_model, doors=new_doors, vehicle_type=new_type)
         vehicle.insert()
 
         #selection = Book.query.order_by(Book.id).all()
@@ -67,12 +119,11 @@ def update_cars(vehicle_id):
     body = request.get_json()
 
     try:
-        vehicle = VehicleModel.query.filter(VehicleModel.id==vehicle_id).one_or_none()
+        vehicle = Vehicle.query.filter(Vehicle.id==vehicle_id).one_or_none()
         
         if vehicle is None:
             abort(404)
        
-        
         if 'brand' in body:
             vehicle.brand = body.get('brand')
         if 'model' in body:
